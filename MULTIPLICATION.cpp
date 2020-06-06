97cf6041e15ceea2a411a56bb44a3ec63272d11a
TAGISWILD <46600294+TAGISWILD@users.noreply.github.com>
2020-06-06 21:24:16 +0530

    Update MULTIPLICATION.cpp

--- MULTIPLICATION.cpp
+++ MULTIPLICATION.cpp
@@ -6,16 +6,17 @@ int main()
 	int num, i;
 	char name[18];
 	FILE *mult;
-	mult = fopen("HISTORY.txt","a+");
-	printf("PLEASE ENTER YOUR NAME BEFORE WE START\nNAME: ");
+	mult = fopen("HISTORY.txt","a+");				//OPENING THE FILE NAMED HISTORY(OPRNING MULT)
+	printf("PLEASE ENTER YOUR NAME BEFORE WE START\nNAME: ");	//TAKING INPUT
 	scanf("%s",&name);
-	printf("ENTER THE NUMBER OF TABLE YOU NEED..\n");
+	printf("ENTER THE NUMBER OF TABLE YOU NEED..\n");		//TAKING INPUT OF NO. OF TABLE
 	scanf("%d",&num);
-	fprintf(mult,"THE NUMBER TAKEN BY %s WAS %d\n***********************************\n",strupr(name),num);
-	for(i=1;i<11;i++)
+	fprintf(mult,"THE NUMBER TAKEN BY %s WAS %d\n***********************************\n",strupr(name),num);//PRINTING THE NAME OF USER AND TABLE
+	for(i=1;i<11;i++)						//DECLARING FOR LOOP
 	{
-		printf("%d * %d = %d\n",num,i,num*i);
-		fprintf(mult,"%d * %d = %d\n",num,i,num*i);
+		printf("%d * %d = %d\n",num,i,num*i);			//PRINTING IN OUTPUT	
+		fprintf(mult,"%d * %d = %d\n",num,i,num*i);		//PRINTING IN HISTORY.TXT
 	}
-	fprintf(mult,"***********************************\n");
+	fprintf(mult,"***********************************\n");		//ENDLINE OF HISTORY
+	fclose(mult);							//CLOSING MULT	
 }
